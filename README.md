de2237f0653394f30ec8d1644c9ed5886387f464
Onkar Singh <onkar.singh@in.bosch.com>
2020-02-11 20:24:20 +0530

    variables updated for docker host

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 ## docker-kafka-zk
 docker compose for 3 node kafka and 3 node zk
 
-- Zookeeper will be available at `$DOCKER_HOST_IP:2181,$DOCKER_HOST_IP:2182,$DOCKER_HOST_IP:2183`
-- Kafka will be available at `$DOCKER_HOST_IP:9092,$DOCKER_HOST_IP:9093,$DOCKER_HOST_IP:9094`
+- Zookeeper available @ `$DOCKER_HOST_DNS:2181,$DOCKER_HOST_DNS:2182,$DOCKER_HOST_DNS:2183`
+- Kafka available @ `$DOCKER_HOST_DNS:9092,$DOCKER_HOST_DNS:9093,$DOCKER_HOST_DNS:9094`
 
 Run with:
 
 ```
-export DOCKER_HOST_IP=127.0.0.1  # your docker machine IP `<docker-machine-ip>`
+export DOCKER_HOST_DNS=127.0.0.1  # your docker machine IP `<docker-machine-ip>`
 
 docker-compose -f docker-compose.yml up
 docker-compose -f docker-compose.yml down
--- kafka-test.sh
+++ kafka-test.sh
@@ -28,11 +28,11 @@ kafka_tests(){
     echo "Testing Kafka"
     topic="testtopic"
     if grep -q kafka3 $1; then replication_factor="3"; else replication_factor="1"; fi
-    for i in 1 2 3 4 5; do echo "trying to create test topic" && kafka-topics --create --topic $topic --replication-factor $replication_factor --partitions 12 --zookeeper $DOCKER_HOST_IP:2181 && break || sleep 5; done
-    for x in {1..100}; do echo $x; done | kafka-console-producer --broker-list $DOCKER_HOST_IP:9092 --topic $topic
-    rows=`kafka-console-consumer --bootstrap-server $DOCKER_HOST_IP:9092 --topic $topic --from-beginning --timeout-ms 2000 | wc -l`
+    for i in 1 2 3 4 5; do echo "trying to create test topic" && kafka-topics --create --topic $topic --replication-factor $replication_factor --partitions 12 --zookeeper $DOCKER_HOST_DNS:2181 && break || sleep 5; done
+    for x in {1..100}; do echo $x; done | kafka-console-producer --broker-list $DOCKER_HOST_DNS:9092 --topic $topic
+    rows=`kafka-console-consumer --bootstrap-server $DOCKER_HOST_DNS:9092 --topic $topic --from-beginning --timeout-ms 2000 | wc -l`
     if [ "$rows" != "100" ]; then
-        kafka-console-consumer --bootstrap-server $DOCKER_HOST_IP:9092 --topic test-topic --from-beginning --timeout-ms 2000 | wc -l
+        kafka-console-consumer --bootstrap-server $DOCKER_HOST_DNS:9092 --topic test-topic --from-beginning --timeout-ms 2000 | wc -l
         exit 1
     else
         echo "Kafka Test Success"
